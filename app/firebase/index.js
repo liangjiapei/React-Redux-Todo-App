fb8c7bfb17a1f5f723d10c11e25184770e3d65c7
Jiapei Liang <liang98@purdue.edu>
2017-04-17 18:09:17 -0400

    Remove firebase credentials from app folder

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -6,9 +6,9 @@ try {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    projectId: "react-redux-todo-app-3b8e0",
+    projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: "555720442883"
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
