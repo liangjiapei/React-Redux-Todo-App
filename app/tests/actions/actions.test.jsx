00c20f3f73f8ef0e90db46fa35446da3fc9a8f53
Jiapei Liang <liang98@purdue.edu>
2017-04-18 12:17:17 -0400

    Added Test files of actions

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,87 @@
+import thunk from 'redux-thunk';
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some search text'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle show completed action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      todo: {
+        id: '123abc',
+        text: 'Anything we like',
+        completed: false,
+        createdAt: 0
+      }
+    };
+    var res = actions.addTodo(action.todo);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todos action object', () => {
+    var todos = [{
+      id: '111',
+      text: 'anything',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 33000
+    }];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate update todo action', () => {
+    var action = {
+      type: 'UPDATE_TODO',
+      id: '123',
+      updates: {completed: false}
+    };
+    var res = actions.updateTodo(action.id, action.updates);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate login action object', () => {
+    const action = {
+      type: 'LOGIN',
+      uid: '123abc'
+    };
+    const res = actions.login(action.uid);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate logout action object', () => {
+    const action = {
+      type: 'LOGOUT'
+    };
+    const res = actions.logout();
+
+    expect(res).toEqual(action);
+  });
+});
